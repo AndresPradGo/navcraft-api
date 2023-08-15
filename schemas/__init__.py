8a18e5b4432c56c28b22a5776d0b4eca54b6b6df
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-15 13:57:47 -0700

    Add schemas package

---
+++ schemas/__init__.py
@@ -0,0 +1,10 @@
+"""
+Schemas package init file
+
+This init module imports all the schemas from the schemas package,
+so they can be imported externaly. 
+
+Usage: 
+- This module will run automatically, whenever the schemas package gets imported.
+
+"""
