cac053b2ba7e94ab6a22f04e64654309bcb3a7cb
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-15 10:19:16 -0700

    Add entry point and init file for main package

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+FastAPI Main Flight Planner API
+
+This module is the entry point of the flight-planner API. 
+It creates the FastAPI app and runs the startup package.
+
+Usage: 
+- Run this module to start the FastAPI development server.
+
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
