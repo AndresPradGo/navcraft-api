10749aa16241b28b538921edb25b22a6b36cd16a
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-15 11:39:14 -0700

    Add models package

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Models package init file
+
+This init module imports all the models from the models package,
+so they can be imported externaly. 
+
+Usage: 
+- This module will run automatically, whenever the models package gets imported.
+
+"""
