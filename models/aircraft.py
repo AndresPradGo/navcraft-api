dcaa6ac77dc4f5dc55a09ed6812aac18883bc217
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-19 11:45:13 -0700

    Add climb/cruise performance data and fuel type  models

--- models/aircraft.py
+++ models/aircraft.py
@@ -69,14 +69,19 @@ class Aircraft(BaseModel):
       in landing distance per knot of tailwind.
     - percent_increase_climb_temperature_c (Integer Column): percent increase in climb
       time, fuel and distance per degree celsius of air temperature above standard.
+    - model_id (Integer Column): foreignkey pointing to the aircraft_models table.
+    - fuel_type_id (Integer Column): foreignkey pointing to the fuel_types table.
     - model (Relationship): defines the many-to-one relationship with the aircraft_models table.
     - flights (Relationship): list of flights with this particular aircraft.
     - performance_decreace_runway_surfaces_percent (Relationship): list of percentages by which 
-      to decrease landing/takeoff performance for different runway surfaces
+      to decrease landing/takeoff performance for different runway surfaces.
     - weight_balance_profiles (Relationship): list of W&B profiles for different categories.
     - baggage_compartments (Relationship): list of baggage compartments the aircraft is equipt with.
     - takeoff_performance_data (Relationship): takeoff performance data table.
-    - arrival_performance_data (Relationship): landing performance data table.
+    - landing_performance_data (Relationship): landing performance data table.
+    - climb_performance_data (Relationship): climb performance data table.
+    - cruise_performance_data (Relationship): cruise performance data table.
+    - fuel_type (Relationship): Defines the many-to-one relationship with the fuel_types table.
     """
 
     __tablename__ = "aircraft"
@@ -125,6 +130,15 @@ class Aircraft(BaseModel):
         ),
         nullable=False
     )
+    fuel_type_id = Column(
+        Integer,
+        ForeignKey(
+            "fuel_types.id",
+            ondelete="RESTRICT",
+            onupdate="CASCADE"
+        ),
+        nullable=False
+    )
 
     model = Relationship("AircraftModel", back_populates="aircraft")
     flights = Relationship(
@@ -157,11 +171,27 @@ class Aircraft(BaseModel):
         passive_deletes=True,
         passive_updates=True
     )
-    arrival_performance_data = Relationship(
-        "ArrivalPerformance",
+    landing_performance_data = Relationship(
+        "LandingPerformance",
         back_populates="aircraft",
         passive_deletes=True,
         passive_updates=True
+    )
+    climb_performance_data = Relationship(
+        "ClimbPerformance",
+        back_populates="aircraft",
+        passive_deletes=True,
+        passive_updates=True
+    )
+    cruise_performance_data = Relationship(
+        "CruisePerformance",
+        back_populates="aircraft",
+        passive_deletes=True,
+        passive_updates=True
+    )
+    fuel_type = Relationship(
+        "FuelType",
+        back_populates="aircraft"
     )
 
     class SurfacePerformanceDecrease(BaseModel):
@@ -170,13 +200,13 @@ class Aircraft(BaseModel):
 
     Attributes:
     - id (Integer Column): table primary key.
-    - registration (String Column):
-    - is_model (Boolean Column): true if the aircraft profile is a general model.
-    - center_of_gravity_in (Decimal Column): center of gravity of empty aircraft 
-      in inches from datum.
-
-    - model (Relationship): defines the many-to-one relationship with the aircraft_models table.
-    - flights (Relationship): list of flights with this particular aircraft.
+    - percent (String Column): percentage by which to decrease landing/takeoff performance 
+      for the runway surface
+    - is_takeoff (Boolean Column): true if the pecentage is for takeoff parformance.
+    - surface_id (Integer Column): foreign key that links to the runway_surfaces table.
+    - aircraft_id (Integer Column): foreign key that links to the aircraft table.
+    - surface (Relationship): defines the many-to-one relationship with the runway_surface table.
+    - aircraft (Relationship): defines the many-to-one relationship with the aicraft table.
     """
 
     __tablename__ = "surfaces_performance_decrease"
@@ -218,6 +248,7 @@ class AircraftCategory(BaseModel):
     Attributes:
     - id (Integer Column): table primary key.
     - category (String Column): aircraft category.
+    - weight_balance_profiles (Relationship): list of W&B profiles with this category.
     """
 
     __tablename__ = "aircraft_categories"
@@ -240,6 +271,7 @@ class WeightBalanceProfile(BaseModel):
     Attributes:
     - id (Integer Column): table primary key.
     - category_id (Integer Column): foreign key representing the id in the aircraft_categories table.
+    - aircraft_id (Integer Column): foreign key that links to the aircraft table.
     - category (Relationship): defines the many_to_one relationship with the aircraft_categories table.
     - aircraft (Relationship): the aircraft for which the W&B profile is for.
     - weight_balance_limits (Relationship): list of W&B boundary limits.
@@ -322,11 +354,11 @@ class WeightBalanceLimit(BaseModel):
 
 class BaggageCompartment(BaseModel):
     """
-    This class defines the database weight_balance_limit model.
+    This class defines the database baggage_compartment model.
 
     Attributes:
     - id (Integer Column): table primary key.
-    - name (String Column): the name of the compartment (e.g. compartment 1, back compartment)
+    - name (String Column): the name of the compartment (e.g. compartment 1, back compartment).
     - arm_in (Decimal Column): the W&B arm of the baggage compartment.
     - weight_limit_lb (Decimal Column): the weight limit in lbs, if any.
     - aircraft_id (Integer Column): foreign key that links to the aircraft table.
@@ -377,7 +409,6 @@ class SeatRow(BaseModel):
     - aircraft_id (Integer Column): foreign key that links to the aircraft table.
     - aircraft (Relationship): defines the many_to_one relationship with the aircraft table.
     - passengers (Relationship): defines the one_to_many relationship with the passengers table.
-
     """
 
     __tablename__ = "seat_rows"
@@ -452,7 +483,7 @@ class TakeoffPerformance(TakeoffLandingPerformance):
     - aircraft (Relationship): Defines the many-to-one relationship with the aircraft table.
     """
 
-    __tablename__ = "takeoff_performance"
+    __tablename__ = "takeoff_performance_data"
 
     aircraft = Relationship(
         "Aircraft",
@@ -460,17 +491,125 @@ class TakeoffPerformance(TakeoffLandingPerformance):
     )
 
 
-class ArrivalPerformance(TakeoffLandingPerformance):
+class LandingPerformance(TakeoffLandingPerformance):
     """
-    This class defines the database arrival_performance model.
+    This class defines the database landing_performance model.
 
     Attributes:
     - aircraft (Relationship): Defines the many-to-one relationship with the aircraft table.
     """
 
-    __tablename__ = "arrival_performance"
+    __tablename__ = "landing_performance_data"
 
     aircraft = Relationship(
         "Aircraft",
-        back_populates="arrival_performance_data"
+        back_populates="landing_performance_data"
     )
+
+
+class ClimbPerformance(BaseModel):
+    """
+    This class defines the database climb_performance model.
+
+    Attributes:
+    - id (Integer Column): table primary key.
+    - weight_lb (Decimal Column): the weight limit in lbs, if any.
+    - pressure_alt_ft (Integer Column): pressure altitude in ft.
+    - temperature_c (Integer Column): air temperature in celsius.
+    - time_min (Integer Column): time to climb from sea level, in minutes.
+    - fuel_gal (Decimal Column): fuel to climb from sea level, in gallons.
+    - distance_nm (Integer Column): distance to climb from sea level, in nautical miles.
+    - aircraft_id (Integer Column): foreignkey pointing to the aircraft table.
+    - aircraft (Relationship): Defines the many-to-one relationship with the aircraft table.
+    """
+
+    __tablename__ = "climb_performance_data"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    weight_lb = Column(DECIMAL(7, 2), nullable=False)
+    pressure_alt_ft = Column(Integer, nullable=False)
+    temperature_c = Column(Integer, nullable=False)
+    time_min = Column(Integer, nullable=False)
+    fuel_gal = Column(DECIMAL(3, 1), nullable=False)
+    distance_nm = Column(Integer, nullable=False)
+    aircraft_id = Column(
+        Integer,
+        ForeignKey(
+            "aircraft.id",
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+        nullable=False
+    )
+
+    aircraft = Relationship(
+        "Aircraft",
+        back_populates="climb_performance_data"
+    )
+
+
+class CruisePerformance(BaseModel):
+    """
+    This class defines the database cruise_performance model.
+
+    Attributes:
+    - id (Integer Column): table primary key.
+    - weight_lb (Decimal Column): the weight limit in lbs, if any.
+    - pressure_alt_ft (Integer Column): pressure altitude in ft.
+    - temperature_c (Integer Column): air temperature in celsius.
+    - bhp_percent (Integer Column): percentage of engine's BHP, use to cruise.
+    - rpm (Integer Column): RPM of the engine during cruise.
+    - ktas (Integer Column): true airspeed in knots.
+    - gph (Decimal Column): gallons per hour of fuel burned during cruise.
+    - aircraft_id (Integer Column): foreignkey pointing to the aircraft table.
+    - aircraft (Relationship): Defines the many-to-one relationship with the aircraft table.
+    """
+
+    __tablename__ = "cruise_performance_data"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    weight_lb = Column(DECIMAL(7, 2), nullable=False)
+    pressure_alt_ft = Column(Integer, nullable=False)
+    temperature_c = Column(Integer, nullable=False)
+    bhp_percent = Column(Integer, nullable=False)
+    rpm = Column(Integer, nullable=False)
+    ktas = Column(Integer, nullable=False)
+    gph = Column(DECIMAL(3, 1), nullable=False)
+    aircraft_id = Column(
+        Integer,
+        ForeignKey(
+            "aircraft.id",
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+        nullable=False
+    )
+
+    aircraft = Relationship(
+        "Aircraft",
+        back_populates="cruise_performance_data"
+    )
+
+    class FuelType(BaseModel):
+        """
+        This class defines the database fuel_type model.
+
+        Attributes:
+        - id (Integer Column): table primary key.
+        - name (String Column): the name of the fuel (e.g. kerosene, 100LL).
+        - density_lb_gal (Decimal Column): density of the fuel in lbs per gallon.
+        - aircraft (Relationship): Defines the one_to_many relationship with the aircraft table.
+        """
+
+        __tablename__ = "fuel_types"
+
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        name = Column(String(50), nullable=False, unique=True)
+        density_lb_gal = Column(DECIMAL(4, 2), nullable=False)
+
+        aircraft = Relationship(
+            "Aircraft",
+            back_populates="fuel_type",
+            passive_deletes=True,
+            passive_updates=True
+        )
