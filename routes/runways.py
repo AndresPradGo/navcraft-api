955ddc85fa5cbfe74fb2667414f136b54f413193
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-24 11:36:25 -0700

    Add post-runway-surface endpoint

--- routes/runways.py
+++ routes/runways.py
@@ -12,7 +12,6 @@ from typing import List
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import text, and_, or_, not_
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
 
 import auth
 import models
@@ -42,9 +41,45 @@ async def get_all_runway_surfaces(
     - HTTPException (500): if there is a server error. 
     """
 
-    try:
-        surfaces = db.query(models.RunwaySurface).all()
-    except IntegrityError:
-        raise common_responses.internal_server_error()
+    return db.query(models.RunwaySurface).all()
 
-    return surfaces
+
+@router.post("/surface", status_code=status.HTTP_201_CREATED, response_model=schemas.RunwaySurfaceReturn)
+async def post_unofficial_waypoint(
+    surface_data: schemas.RunwaySurfaceData,
+    db: Session = Depends(get_db),
+    _: schemas.TokenData = Depends(auth.validate_admin_user)
+):
+    """
+    Post Runway Surface Endpoint.
+
+    Parameters: 
+    - waypoint (dict): the runway surface object to be added.
+
+    Returns: 
+    Dic: dictionary with the runway surface data.
+
+    Raise:
+    - HTTPException (400): if runway surface already exists.
+    - HTTPException (500): if there is a server error. 
+    """
+
+    surface_exists = db.query(models.RunwaySurface).filter(
+        models.RunwaySurface.surface == surface_data.surface).first()
+
+    if surface_exists:
+        msg = f"{surface_data.surface} is already in the database, please enter a different surface, or edit the existing one."
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=msg
+        )
+
+    new_surface = models.User(
+        surface=surface_data.surface,
+        performance_level=surface_data.performance_level
+    )
+    db.add(new_surface)
+    db.commit()
+    db.refresh(new_surface)
+
+    return new_surface
