53d40d686f29d915e3619bf41edd00fd678d92d7
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-08-30 19:29:50 -0700

    Add get, put, delete fuel type endpoints

--- routes/aircraft.py
+++ routes/aircraft.py
@@ -8,7 +8,10 @@ Usage:
 
 """
 
+from typing import List, Optional
+
 from fastapi import APIRouter, Depends, status, HTTPException, UploadFile
+from sqlalchemy import and_, or_, not_
 from sqlalchemy.orm import Session
 
 import auth
@@ -18,6 +21,35 @@ from utils import common_responses, csv_tools as csv
 from utils.db import get_db
 
 router = APIRouter(tags=["Aircraft"])
+
+
+@router.get("/fuel-type", status_code=status.HTTP_200_OK, response_model=List[schemas.FuelTypeReturn])
+async def get_fuel_types(
+    id: Optional[int] = 0,
+    db: Session = Depends(get_db),
+    _: schemas.TokenData = Depends(auth.validate_user)
+):
+    """
+    Get Fuel Types Endpoint.
+
+    Parameters: 
+    - id (int): fuel type id, for returning only 1.
+
+    Returns: 
+    - List: list of dictionaries with the fuel types.
+
+    Raise:
+    - HTTPException (400): if fuel type doesn't exists.
+    - HTTPException (401): if user is not authenticated.
+    - HTTPException (500): if there is a server error. 
+    """
+
+    fuel_types = db.query(models.FuelType).filter(or_(
+        not_(id),
+        models.FuelType.id == id
+    )).all()
+
+    return [fuel_type.__dict__ for fuel_type in fuel_types]
 
 
 @router.post("/fuel-type", status_code=status.HTTP_201_CREATED, response_model=schemas.FuelTypeReturn)
@@ -58,3 +90,90 @@ async def post_fuel_type(
     db.refresh(new_fuel_type)
 
     return new_fuel_type
+
+
+@router.put("/fuel-type/{id}", status_code=status.HTTP_201_CREATED, response_model=schemas.FuelTypeReturn)
+async def edit_fuel_type(
+    id: int,
+    fuel_type: schemas.FuelTypeData,
+    db: Session = Depends(get_db),
+    _: schemas.TokenData = Depends(auth.validate_admin_user)
+):
+    """
+    Edit Fuel Type Endpoint.
+
+    Parameters: 
+    - id (int): fuel type id.
+    - fuel_type (dict): the fuel type data to be added.
+
+    Returns: 
+    - Dic: dictionary with the new fuel type data.
+
+    Raise:
+    - HTTPException (400): if fuel type doesn't exists.
+    - HTTPException (401): if user is not admin user.
+    - HTTPException (500): if there is a server error. 
+    """
+
+    # Check id is valid
+    fuelt_type_query = db.query(models.FuelType).filter(
+        models.FuelType.id == id)
+
+    if not fuelt_type_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Fuel type with id {id} doesn't exists in the database."
+        )
+
+    # Check if fuel type with same name exists
+    fuelt_type_exists = db.query(models.FuelType).filter(and_(
+        models.FuelType.name == fuel_type.name,
+        not_(models.FuelType.id == id)
+    )).first()
+    if fuelt_type_exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"{fuel_type.name} fuel already exists in the database."
+        )
+    # Edit fuel type
+    fuelt_type_query.update(fuel_type.model_dump())
+    db.commit()
+
+    # Return fuel type data
+    return fuelt_type_query.first().__dict__
+
+
+@router.delete("/fuel-type/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def edit_fuel_type(
+    id: int,
+    db: Session = Depends(get_db),
+    _: schemas.TokenData = Depends(auth.validate_admin_user)
+):
+    """
+    Delete Fuel Type Endpoint.
+
+    Parameters: 
+    - id (int): fuel type id.
+
+    Returns: None
+
+    Raise:
+    - HTTPException (400): if fuel type doesn't exists.
+    - HTTPException (401): if user is not admin user.
+    - HTTPException (500): if there is a server error. 
+    """
+
+    # Check if fuel type exists.
+    fuelt_type_query = db.query(models.FuelType).filter(
+        models.FuelType.id == id)
+    if not fuelt_type_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Fuel type with id {id} doesn't exists in the database."
+        )
+
+    # Delete fuel type
+    deleted = fuelt_type_query.delete(synchronize_session=False)
+    if not deleted:
+        raise common_responses.internal_server_error()
+    db.commit()
