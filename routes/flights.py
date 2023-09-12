67d85b7fd3f3ed16ed37a76f1134782ea9eb63ce
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-09-12 15:49:05 -0700

    Add delete flight-leg endpoint

--- routes/flights.py
+++ routes/flights.py
@@ -27,6 +27,61 @@ from utils.functions import clean_string, get_user_id_from_email
 router = APIRouter(tags=["Flights"])
 
 
+def get_basic_flight_data_for_return(flight_id: int, db_session: Session, user_id: int):
+    flight = db_session.query(models.Flight).filter(and_(
+        models.Flight.id == flight_id,
+        models.Flight.pilot_id == user_id
+    )).first()
+
+    departure = db_session.query(models.Departure, models.Aerodrome)\
+        .join(models.Aerodrome, models.Departure.aerodrome_id == models.Aerodrome.id)\
+        .filter(models.Departure.flight_id == flight_id).first()
+
+    arrival = db_session.query(models.Arrival, models.Aerodrome)\
+        .join(models.Aerodrome, models.Arrival.aerodrome_id == models.Aerodrome.id)\
+        .filter(models.Arrival.flight_id == flight_id).first()
+
+    legs = db_session.query(models.Leg, models.FlightWaypoint, models.Waypoint)\
+        .outerjoin(models.FlightWaypoint, models.Leg.id == models.FlightWaypoint.leg_id)\
+        .outerjoin(models.Waypoint, models.FlightWaypoint.waypoint_id == models.Waypoint.id)\
+        .filter(models.Leg.flight_id == flight_id).order_by(models.Leg.sequence).all()
+
+    return {
+        "id": flight.id,
+        "departure_time": pytz.timezone('UTC').localize((flight.departure_time)),
+        "aircraft_id": flight.aircraft_id,
+        "departure_aerodrome_id": departure[1].id,
+        "departure_aerodrome_is_private": departure[1].user_waypoint is not None,
+        "arrival_aerodrome_id": arrival[1].id,
+        "arrival_aerodrome_is_private": arrival[1].user_waypoint is not None,
+        "legs": [{
+            "id": leg.id,
+            "sequence": leg.sequence,
+            "waypoint": {
+                "id": wp.id,
+                "code": flight_wp.code,
+                "lat_degrees": wp.lat_degrees,
+                "lat_minutes": wp.lat_minutes,
+                "lat_seconds": wp.lat_seconds,
+                "lat_direction": wp.lat_direction,
+                "lon_degrees": wp.lon_degrees,
+                "lon_minutes": wp.lon_minutes,
+                "lon_seconds": wp.lon_seconds,
+                "lon_direction": wp.lon_direction,
+                "magnetic_variation": wp.magnetic_variation
+            } if flight_wp is not None else None,
+            "altitude_ft": leg.altitude_ft,
+            "temperature_c": leg.temperature_c,
+            "wind_magnitude_knot": leg.wind_magnitude_knot,
+            "wind_direction": leg.wind_direction,
+            "weather_valid_from": pytz.timezone('UTC').localize((leg.weather_valid_from))
+            if leg.weather_valid_from is not None else None,
+            "weather_valid_to": pytz.timezone('UTC').localize((leg.weather_valid_to))
+            if leg.weather_valid_to is not None else None,
+        } for leg, flight_wp, wp in legs]
+    }
+
+
 @router.get(
     "/status",
     status_code=status.HTTP_200_OK,
@@ -328,58 +383,11 @@ async def post_new_leg(
     db_session.commit()
 
     # Return flight data
-    flight = db_session.query(models.Flight).filter(and_(
-        models.Flight.id == flight_id,
-        models.Flight.pilot_id == user_id
-    )).first()
-
-    departure = db_session.query(models.Departure, models.Aerodrome)\
-        .join(models.Aerodrome, models.Departure.aerodrome_id == models.Aerodrome.id)\
-        .filter(models.Departure.flight_id == flight_id).first()
-
-    arrival = db_session.query(models.Arrival, models.Aerodrome)\
-        .join(models.Aerodrome, models.Arrival.aerodrome_id == models.Aerodrome.id)\
-        .filter(models.Arrival.flight_id == flight_id).first()
-
-    legs = db_session.query(models.Leg, models.FlightWaypoint, models.Waypoint)\
-        .outerjoin(models.FlightWaypoint, models.Leg.id == models.FlightWaypoint.leg_id)\
-        .outerjoin(models.Waypoint, models.FlightWaypoint.waypoint_id == models.Waypoint.id)\
-        .filter(models.Leg.flight_id == flight_id).order_by(models.Leg.sequence).all()
-
-    return {
-        "id": flight.id,
-        "departure_time": pytz.timezone('UTC').localize((flight.departure_time)),
-        "aircraft_id": flight.aircraft_id,
-        "departure_aerodrome_id": departure[1].id,
-        "departure_aerodrome_is_private": departure[1].user_waypoint is not None,
-        "arrival_aerodrome_id": arrival[1].id,
-        "arrival_aerodrome_is_private": arrival[1].user_waypoint is not None,
-        "legs": [{
-            "id": leg.id,
-            "sequence": leg.sequence,
-            "waypoint": {
-                "id": wp.id,
-                "code": flight_wp.code,
-                "lat_degrees": wp.lat_degrees,
-                "lat_minutes": wp.lat_minutes,
-                "lat_seconds": wp.lat_seconds,
-                "lat_direction": wp.lat_direction,
-                "lon_degrees": wp.lon_degrees,
-                "lon_minutes": wp.lon_minutes,
-                "lon_seconds": wp.lon_seconds,
-                "lon_direction": wp.lon_direction,
-                "magnetic_variation": wp.magnetic_variation
-            } if flight_wp is not None else None,
-            "altitude_ft": leg.altitude_ft,
-            "temperature_c": leg.temperature_c,
-            "wind_magnitude_knot": leg.wind_magnitude_knot,
-            "wind_direction": leg.wind_direction,
-            "weather_valid_from": pytz.timezone('UTC').localize((leg.weather_valid_from))
-            if leg.weather_valid_from is not None else None,
-            "weather_valid_to": pytz.timezone('UTC').localize((leg.weather_valid_to))
-            if leg.weather_valid_to is not None else None,
-        } for leg, flight_wp, wp in legs]
-    }
+    return get_basic_flight_data_for_return(
+        flight_id=flight_id,
+        db_session=db_session,
+        user_id=user_id
+    )
 
 
 @router.post(
@@ -484,6 +492,95 @@ async def delete_flight(
         raise common_responses.internal_server_error()
 
     db_session.commit()
+
+
+@router.delete(
+    "/leg/{leg_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=schemas.NewFlightReturn
+)
+async def delete_flight_leg(
+    leg_id: int,
+    db_session: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(auth.validate_user)
+):
+    """
+    Delete Flight Leg.
+
+    Parameters: 
+    leg_id (int): leg id.
+
+    Returns: None
+
+    Raise:
+    - HTTPException (401): invalid credentials.
+    - HTTPException (404): status not found.
+    - HTTPException (500): if there is a server error. 
+    """
+
+    # Check leg exists and user has permission to delete
+    user_id = await get_user_id_from_email(email=current_user.email, db_session=db_session)
+    leg_query_results = db_session.query(models.Leg, models.Flight)\
+        .join(models.Flight, models.Leg.flight_id == models.Flight.id)\
+        .filter(and_(models.Leg.id == leg_id, models.Flight.pilot_id == user_id)).first()
+
+    if leg_query_results is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Leg not found."
+        )
+
+    flight_id = leg_query_results[1].id
+
+    # Check Leg is not the last leg
+    leg_ids = [
+        leg[0] for leg in db_session.query(models.Leg.id).filter(
+            models.Leg.flight_id == flight_id
+        ).order_by(models.Leg.sequence).all()
+    ]
+    leg_is_last = leg_id == leg_ids[-1]
+    if leg_is_last:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The last leg of the flight cannot be deleted."
+        )
+
+    # Find waypoin ID
+    waypoint_id = db_session.query(models.FlightWaypoint.waypoint_id).filter(
+        models.FlightWaypoint.leg_id == leg_id).first()[0]
+
+    # Delete leg and waypoint
+    leg_deleted = db_session.query(models.Leg).filter(
+        models.Leg.id == leg_id).delete(synchronize_session=False)
+
+    waypoint_deleted = db_session.query(models.Waypoint).filter_by(
+        id=waypoint_id).delete(synchronize_session=False)
+
+    if not leg_deleted or not waypoint_deleted:
+        raise common_responses.internal_server_error()
+
+    db_session.commit()
+
+    # Update sequence
+    new_leg_sequence_list = [
+        {"id": leg.id, "sequence": seq + 1} for seq, leg in enumerate(
+            db_session.query(models.Leg).filter_by(flight_id=flight_id)
+            .order_by(models.Leg.sequence).all()
+        )
+    ]
+
+    for new_leg_sequence in new_leg_sequence_list:
+        db_session.query(models.Leg).filter_by(
+            id=new_leg_sequence["id"]).update(new_leg_sequence)
+
+    db_session.commit()
+
+    # Return new flight data
+    return get_basic_flight_data_for_return(
+        flight_id=flight_id,
+        db_session=db_session,
+        user_id=user_id
+    )
 
 
 @router.delete("/status/{status_id}", status_code=status.HTTP_204_NO_CONTENT)
