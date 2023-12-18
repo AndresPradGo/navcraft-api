2cb36fd0d736c959f163cde69e7ad22739f89070
Andres Pradilla <ae.pradillagomez@gmail.com>
2023-12-17 17:32:54 -0800

    Fix bug in adjusted LND/TO distances calculations

--- functions/aircraft_performance.py
+++ functions/aircraft_performance.py
@@ -213,10 +213,10 @@ def get_landing_takeoff_data(
         result["obstacle_clearance_ft"] / 100
 
     # Apply runway surface corrections
-    result["adjusted_obstacle_clearance_ft"] = result["obstacle_clearance_ft"] + \
-        surface_correction * result["groundroll_ft"] / 100
-    result["adjusted_groundroll_ft"] = result["groundroll_ft"] + \
-        surface_correction * result["groundroll_ft"] / 100
+    result["adjusted_obstacle_clearance_ft"] = result["adjusted_obstacle_clearance_ft"] + \
+        surface_correction * result["adjusted_groundroll_ft"] / 100
+    result["adjusted_groundroll_ft"] = result["adjusted_groundroll_ft"] + \
+        surface_correction * result["adjusted_groundroll_ft"] / 100
 
     # Pre-process results and return
     result["groundroll_ft"] = int(round(result["groundroll_ft"], 0))
@@ -230,7 +230,6 @@ def get_landing_takeoff_data(
 
     new_input_targets = {
         value: adjusted_targets[i] for i, value in enumerate(inputs)}
-
     return result, new_input_targets
 
 
